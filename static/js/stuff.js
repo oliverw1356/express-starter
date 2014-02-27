b71170c7959b59b01cb7a7d3b032dbe25adfe4c1
oliverw1356 <oliverw1356@yahoo.com>
2014-02-26 18:00:18 -0800

    edit

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -10,7 +10,7 @@ $('#say_hello').click(function() {
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
   //WRITE CODE HERE
-  $("houdini_text").hide();
+  $("Houdini").hide();
 });
 
 $('#reappear').click(function() {
