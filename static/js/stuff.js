3d64ecc4451ff37514fa50c6868e28de8bbfbd05
oliverw1356 <oliverw1356@yahoo.com>
2014-02-26 17:56:22 -0800

    edit

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -20,7 +20,9 @@ $('#reappear').click(function() {
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
-// WRITE CODE HERE
+$('#pink').click(function() {
+	$("pink_text").css('color','pink');
+});
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
